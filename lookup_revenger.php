a5b8a644b201c3fb42f15c869188e55baa90ef47
Min-Cheng <wastiphiby@gmail.com>
2022-12-25 07:08:39 -0500

    Update the repository

--- lookup_revenger.php
+++
@@ -1,8 +0,0 @@
-<h1> 🕶⬆</h1>
-<form>
-    <input type="text" name="target">
-    <input type="submit" value="🕶">
-</form>
-<pre>
-<?php if (isset($_GET['target']) && !preg_match("/\||&|<|>|`|;|\n|flag/", $_GET['target'])) system("nslookup -type=hinfo " . $_GET['target']) ?>
-</pre>
